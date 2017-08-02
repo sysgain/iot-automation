313a4c99ca3d8bad8dfbae177c102ca107ef777c
SrikalaRekapalli <srekapalli@sysgain.com>
2017-08-02 11:38:48 +0530

    Delete defaultdb.ps1

--- scripts/defaultdb.ps1
+++
@@ -1,3 +0,0 @@
-$client = new-object System.Net.WebClient
-$client.DownloadFile("https://raw.githubusercontent.com/sysgain/iot-automation/master/scripts/SQLQuery2.sql","C:\test.sql")
-sqlcmd.exe -S sqlvm -U sqluser -P Sysgain@1234 -i C:\test.sql -o "c:\Testoutput.txt"
