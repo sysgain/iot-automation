054dfbb83ee7a06bb0925a50a6dce4789a46dd9e
ooha-m <omanduri@sysgain.com>
2017-08-02 16:45:40 +0530

    Update defaultdb.ps1

--- scripts/defaultdb.ps1
+++ scripts/defaultdb.ps1
@@ -1,12 +1,12 @@
 param(
 [string] $sqlQueryUrl = "$1",
 [string] $sqlservername = "$2",
-[string] $adminUsername = "$3",
-[string] $adminPassword = "$4",
+[string] $sqlAuthenticationLogin = "$3",
+[string] $sqlAuthenticationPassword = "$4",
 [string] $databaseName = "$5"
 )
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
 $client = New-Object System.Net.WebClient
 $client.DownloadFile($sqlQueryUrl,"C:\createdb.sql")
 cd C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn
-sqlcmd.exe -S $sqlservername -U $adminUsername -P $adminPassword -i C:\createdb.sql -o "C:\Testoutput.txt"
+sqlcmd.exe -S $sqlservername -U $sqlAuthenticationLogin -P $sqlAuthenticationPassword -i C:\createdb.sql -o "C:\Testoutput.txt"
