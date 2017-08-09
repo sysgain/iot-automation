894d090cdddc090295107964c083f48d997a1caa
unknown <srekapalli@sysgain.com>
2017-08-09 17:08:35 +0530

    update

--- scripts/simulatorsetup.ps1
+++ scripts/simulatorsetup.ps1
@@ -3,8 +3,10 @@ param(
 [string] $dataserviceUrl = "$2",
 [string] $PIWebApisimulatorUrl = "$3"
 )
+$PIAFservicesUrl = "https://projectiot.blob.core.windows.net/iotp2/PI-AF-Services_2017-SP1a_.exe"
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
 $client = new-object System.Net.WebClient
+$client.DownloadFile($PIAFservicesUrl,"C:\PI-AF-Services_2017-SP1a_.exe")
 $client.DownloadFile($simulatorUrl,"C:\SimulatorSetup.msi")
 Start-Sleep -s 12
 $client.DownloadFile($dataserviceUrl,"C:\DataServiceAppSetup.msi")
