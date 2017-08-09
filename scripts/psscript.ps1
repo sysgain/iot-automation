d87b55c5572851c131fde7864d84b1af8df61112
unknown <srekapalli@sysgain.com>
2017-08-09 17:18:19 +0530

    up

--- scripts/psscript.ps1
+++ scripts/psscript.ps1
@@ -6,6 +6,11 @@ param
 
 [string] $password = "$3"
 )
+$PIdataArchiveUrl = "https://projectiot.blob.core.windows.net/iotp2/PI-Data-Archive_2017-SP1_.exe"
+$PIIntegratorBAUrl = "https://projectiot.blob.core.windows.net/iotp2/OSIsoft.PIIntegratorBA_DW_5000_1.2.0.104_.exe"
+$client = new-object System.Net.WebClient
+$client.DownloadFile($PIdataArchiveUrl,"C:\PI-Data-Archive_2017-SP1_.exe")
+$client.DownloadFile($PIIntegratorBAUrl,"C:\OSIsoft.PIIntegratorBA_DW_5000_1.2.0.104_.exe")
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 $usernamenNew = $domainName+"\"+${username}
 Set-DnsClient `
