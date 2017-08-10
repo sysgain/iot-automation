770b7db284c0c7d9e1cdb2a2c0d57de93f492da2
unknown <srekapalli@sysgain.com>
2017-08-10 15:06:08 +0530

    up

--- scripts/powerbidesktop.ps1
+++ scripts/powerbidesktop.ps1
@@ -2,7 +2,7 @@
 # https://projectiot.blob.core.windows.net/iotp2/PBIDesktop_x64.msi
 param(
 [string] $powerBItemplates = "$1",
-[string] $powerbidesktop = "$2",
+[string] $powerbidesktop = "$2"
 )
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
 $client = new-object System.Net.WebClient
--- scripts/psscript.ps1
+++ scripts/psscript.ps1
@@ -13,6 +13,7 @@ $client = new-object System.Net.WebClient
 $client.DownloadFile($PIdataArchiveUrl,"C:\PI-Data-Archive_2017-SP1_.exe")
 $client.DownloadFile($PIIntegratorBAUrl,"C:\OSIsoft.PIIntegratorBA_DW_5000_1.2.0.104_.exe")
 $client.DownloadFile($PIlicenseUrl,"C:\pilicense.dat")
+$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 $usernamenNew = $domainName+"\"+${username}
 Set-DnsClient `
