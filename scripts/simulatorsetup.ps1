2de041df33a936ab585437c4bbf83ce8c73f3faa
unknown <srekapalli@sysgain.com>
2017-08-14 14:25:42 +0530

    up

--- scripts/simulatorsetup.ps1
+++ scripts/simulatorsetup.ps1
@@ -8,6 +8,7 @@ param(
 )
 $PIAFservicesUrl = "https://projectiot.blob.core.windows.net/iotp2/PI-AF-Services_2017-SP1a_.exe"
 $PIAFInstallationFile = "https://projectiot.blob.core.windows.net/iotp2/piafinstallation.ps1"
+$PITemplates = "https://projectiot.blob.core.windows.net/iotp2/PITemplates.zip"
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
 $client = new-object System.Net.WebClient
 $client.DownloadFile($PIAFservicesUrl,"C:\PI-AF-Services_2017-SP1a_.exe")
@@ -18,4 +19,6 @@ Start-Sleep -s 12
 $client.DownloadFile($PIWebApisimulatorUrl,"C:\PIWebApiSimulatorSetup.msi")
 Start-Sleep -s 12 
 $client.DownloadFile($PIAFInstallationFile,"C:\piafinstallation.ps1")
+Start-Sleep -s 12
+$client.DownloadFile($PITemplates,"C:\PITemplates.zip") 
 #C:\PI-AF-Services_2017-SP1a_.exe ADDLOCAL=ALL AFSERVICEACCOUNT=PIAFSQLSERVER\adminuser AFSERVICEPASSWORD=Password@1234 FDSQLDBSERVER=PIAFSQLSERVER /quiet
