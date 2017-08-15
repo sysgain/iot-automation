c6f6c338a5c4bda33b4f3e36bf7fefd18291e6ba
unknown <srekapalli@sysgain.com>
2017-08-15 09:23:32 +0530

    up

--- scripts/simulatorsetup.ps1
+++ scripts/simulatorsetup.ps1
@@ -1,16 +1,7 @@
 param(
 [string] $simulatorUrl = "$1",
 [string] $dataserviceUrl = "$2",
-[string] $PIWebApisimulatorUrl = "$3",
-[string] $adminuser = "$4",
-[string] $adminPassword = "$5",
-[string] $sqlservername = "$6",
-[string] $azuresqlservername = "$7",
-[string] $azuresqlserverdbname = "$8",
-[string] $sqlusername = "$9",
-[string] $sqlpassword = "$10",
-[string] $storageaccountname = "$11",
-[string] $storageaccountkey = "$12"
+[string] $PIWebApisimulatorUrl = "$3"
 )
 $PIAFservicesUrl = "https://projectiot.blob.core.windows.net/iotp2/PI-AF-Services_2017-SP1a_.exe"
 $PIAFInstallationFile = "https://projectiot.blob.core.windows.net/iotp2/piafinstallation.ps1"
