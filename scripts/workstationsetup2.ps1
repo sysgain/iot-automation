9f4b7d27ffab5dd6065e8ca44614e97782f52bd0
unknown <srekapalli@sysgain.com>
2017-08-18 11:51:16 +0530

    up

--- scripts/workstationsetup2.ps1
+++ scripts/workstationsetup2.ps1
@@ -4,12 +4,10 @@ param(
 [string] $ChefServerFqdn = "$3",
 [string] $organizationName= "$4",
 [string] $PIAFSQLIP= "$5",
-[string] $PIDAIP= "$6",
-[string] $PIBAIP= "$7",
-[string] $adIP= "$8",
-[string] $bastionFQDN= "$9",
-[string] $workstationFQDN= "$10",
-[string] $PrivateKey= "$11"
+[string] $PIBAIP= "$6",
+[string] $bastionFQDN= "$7",
+[string] $workstationFQDN= "$8",
+[string] $PrivateKey= "$9"
 )
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
 New-Item C:\Deploy\ -type directory
@@ -27,24 +25,16 @@ knife cookbook upload --config c:\Users\chef-repo\.chef\knife.rb --server-url ht
 knife cookbook upload --config c:\Users\chef-repo\.chef\knife.rb --server-url https://$ChefServerFqdn/organizations/$organizationName/ yum-epel dmg seven_zip mingw build-essential  git
 knife bootstrap windows winrm localhost --config c:\Users\chef-repo\.chef\knife.rb -x $adminusername  -P $adminPassword -N $workstationFQDN 
 knife bootstrap windows winrm  $PIAFSQLIP --config c:\Users\chef-repo\.chef\knife.rb -x $adminusername  -P $adminPassword -N $PIAFSQLIP
-knife bootstrap windows winrm  $PIDAIP --config c:\Users\chef-repo\.chef\knife.rb -x $adminusername  -P $adminPassword -N $PIDAIP
 knife bootstrap windows winrm  $PIBAIP --config c:\Users\chef-repo\.chef\knife.rb -x $adminusername  -P $adminPassword -N $PIBAIP
-knife bootstrap windows winrm  $adIP --config c:\Users\chef-repo\.chef\knife.rb -x $adminusername  -P $adminPassword -N $adIP
 knife bootstrap windows winrm  $bastionFQDN --config c:\Users\chef-repo\.chef\knife.rb -x $adminusername  -P $adminPassword -N $bastionFQDN
 knife node run_list add --config c:\users\chef-repo\.chef\knife.rb --server-url https://$ChefServerFqdn/organizations/$organizationName/ $workstationFQDN recipe[audit]
 knife node run_list add --config c:\users\chef-repo\.chef\knife.rb --server-url https://$ChefServerFqdn/organizations/$organizationName/ $PIAFSQLIP recipe[git]
 knife node run_list add --config c:\users\chef-repo\.chef\knife.rb --server-url https://$ChefServerFqdn/organizations/$organizationName/ $PIAFSQLIP recipe[audit]
-knife node run_list add --config c:\users\chef-repo\.chef\knife.rb --server-url https://$ChefServerFqdn/organizations/$organizationName/ $PIDAIP recipe[git]
-knife node run_list add --config c:\users\chef-repo\.chef\knife.rb --server-url https://$ChefServerFqdn/organizations/$organizationName/ $PIDAIP recipe[audit]
 knife node run_list add --config c:\users\chef-repo\.chef\knife.rb --server-url https://$ChefServerFqdn/organizations/$organizationName/ $PIBAIP recipe[git]
 knife node run_list add --config c:\users\chef-repo\.chef\knife.rb --server-url https://$ChefServerFqdn/organizations/$organizationName/ $PIBAIP recipe[audit]
-knife node run_list add --config c:\users\chef-repo\.chef\knife.rb --server-url https://$ChefServerFqdn/organizations/$organizationName/ $adIP recipe[git]
-knife node run_list add --config c:\users\chef-repo\.chef\knife.rb --server-url https://$ChefServerFqdn/organizations/$organizationName/ $adIP recipe[audit]
 knife node run_list add --config c:\users\chef-repo\.chef\knife.rb --server-url https://$ChefServerFqdn/organizations/$organizationName/ $bastionFQDN recipe[git]
 knife node run_list add --config c:\users\chef-repo\.chef\knife.rb --server-url https://$ChefServerFqdn/organizations/$organizationName/ $bastionFQDN recipe[audit]
 chef-client
 knife winrm name:$PIAFSQLIP -a ipaddress -x ${adminUsername}  -P $adminPassword --config c:\users\chef-repo\.chef\knife.rb chef-client
-knife winrm name:$PIDAIP -a ipaddress -x ${adminUsername}  -P $adminPassword --config c:\users\chef-repo\.chef\knife.rb chef-client
 knife winrm name:$PIBAIP -a ipaddress -x ${adminUsername}  -P $adminPassword --config c:\users\chef-repo\.chef\knife.rb chef-client
-knife winrm name:$adIP -a ipaddress -x ${adminUsername}  -P $adminPassword --config c:\users\chef-repo\.chef\knife.rb chef-client
 knife winrm name:$bastionFQDN -a ipaddress -x ${adminUsername}  -P $adminPassword --config c:\users\chef-repo\.chef\knife.rb chef-client
