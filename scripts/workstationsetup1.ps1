ae2ddbee700d1e00e9f01a038f24c605ddc63c49
unknown <srekapalli@sysgain.com>
2017-08-31 20:09:21 +0530

    up

--- scripts/workstationsetup1.ps1
+++ scripts/workstationsetup1.ps1
@@ -3,5 +3,5 @@ $LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object
 . { iwr -useb https://projectiot.blob.core.windows.net/iotp2/install.ps1 } | iex; install -project chefdk -channel stable -version 2.0.26
 setx PATH "$env:path;C:\opscode\chefDK\embedded\bin\" -m
 Invoke-WebRequest -Uri https://aztdrepo.blob.core.windows.net/chefautomate-testdrive/putty-64bit-0.70-installer.msi -OutFile c:/users/Putty.msi 
-Start-Process c:/Users/Putty.msi /qn -Wait
+Start-Process c:/Users/Putty.msi  /qn -Wait
 Restart-Computer
